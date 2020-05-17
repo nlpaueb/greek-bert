5f719da0859a74ee253eea531f91ef47e7ff2816
John Koutsikakis <jkoutsikakis@gmail.com>
2020-05-18 02:17:12 +0300

    [DEV] Update CLI.

--- examples/__main__.py
+++ examples/__main__.py
@@ -2,6 +2,7 @@ import click
 
 from .xnli.__main__ import xnli
 from .ud.__main__ import ud
+from .ner.__main__ import ner
 
 
 @click.group()
@@ -11,6 +12,7 @@ def cli():
 
 cli.add_command(xnli)
 cli.add_command(ud)
+cli.add_command(ner)
 
 if __name__ == '__main__':
     cli()
